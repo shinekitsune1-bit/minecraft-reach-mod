aec62db911e5764210bb9ad902ee6f19f806bcdb
MUSIXSHINE <shinekitsune1@gmail.com>
2026-09-08 17:25:54 +0500

    Update KeyBindings.java

--- src/main/java/com/shinekitsune/reachmod/keybinding/KeyBindings.java
+++ src/main/java/com/shinekitsune/reachmod/keybinding/KeyBindings.java
@@ -1,28 +1,42 @@
 package com.shinekitsune.reachmod.keybinding;
 
+import com.shinekitsune.reachmod.ReachMod;
 import com.shinekitsune.reachmod.screen.ReachModScreen;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
-	public static KeyMapping openConfigKeyBinding;
 
-	public static void init() {
-		openConfigKeyBinding = KeyBindingHelper.registerKeyBinding(
-			new KeyMapping(
-				"key.reach_mod.open_config",
-				GLFW.GLFW_KEY_R,
-				"category.reach_mod"
-			)
-		);
+    public static KeyBinding openConfigKeyBinding;
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (openConfigKeyBinding.consumeClick()) {
-				Minecraft.getInstance().setScreen(new ReachModScreen());
-			}
-		});
-	}
-}
+    public static void register() {
+        openConfigKeyBinding = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.reachmod.open_config",
+                        GLFW.GLFW_KEY_R,
+                        "category.reachmod"
+                )
+        );
+
+        ReachMod.CLIENT_EXECUTOR.execute(() -> {
+            while (true) {
+                if (openConfigKeyBinding.wasPressed()) {
+                    MinecraftClient client = MinecraftClient.getInstance();
+
+                    if (client.currentScreen == null) {
+                        client.setScreen(new ReachModScreen());
+                    }
+                }
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        });
+    }
+						 }

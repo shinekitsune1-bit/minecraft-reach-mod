fe7616e50c8fa916d4400e95b5e036a2e3161f1b
MUSIXSHINE <shinekitsune1@gmail.com>
2026-09-08 17:23:31 +0500

    Update ReachModScreen.java

--- src/main/java/com/shinekitsune/reachmod/screen/ReachModScreen.java
+++ src/main/java/com/shinekitsune/reachmod/screen/ReachModScreen.java
@@ -1,107 +1,113 @@
 package com.shinekitsune.reachmod.screen;
 
 import com.shinekitsune.reachmod.ReachConfig;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Checkbox;
-import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CheckboxWidget;
+import net.minecraft.text.Text;
 
 public class ReachModScreen extends Screen {
-	private static final int SLIDER_WIDTH = 150;
-	private static final int SLIDER_HEIGHT = 20;
-	private static final int SLIDER_X = 50;
-	private static final int SLIDER_Y = 60;
 
-	private Checkbox enabledCheckbox;
-	private Button decreaseButton;
-	private Button increaseButton;
-	private int sliderValue;
+    private static final int SLIDER_WIDTH = 150;
+    private static final int SLIDER_HEIGHT = 20;
+    private static final int SLIDER_X = 50;
+    private static final int SLIDER_Y = 60;
 
-	public ReachModScreen() {
-		super(Component.literal("Reach Mod Configuration"));
-		this.sliderValue = (int) ReachConfig.reachDistance;
-	}
+    private CheckboxWidget enabledCheckbox;
+    private ButtonWidget decreaseButton;
+    private ButtonWidget increaseButton;
+    private int sliderValue;
 
-	@Override
-	protected void init() {
-		super.init();
+    public ReachModScreen() {
+        super(Text.literal("Reach Mod Configuration"));
+        this.sliderValue = (int) ReachConfig.reachDistance;
+    }
 
-		// Title
-		this.addRenderableWidget(new Button.Builder(
-				Component.literal("Reach Mod Settings"),
-				button -> {}
-		).pos(this.width / 2 - 75, 10).width(150).build());
+    @Override
+    protected void init() {
+        super.init();
 
-		// Toggle Enable/Disable
-		this.enabledCheckbox = new Checkbox(
-				SLIDER_X,
-				40,
-				150,
-				20,
-				Component.literal("Reach Mod Enabled"),
-				ReachConfig.enabled
-		);
-		this.addRenderableWidget(this.enabledCheckbox);
+        this.addDrawableChild(ButtonWidget.builder(
+                Text.literal("Reach Mod Settings"),
+                button -> {}
+        ).dimensions(this.width / 2 - 75, 10, 150, 20).build());
 
-		// Decrease Button
-		this.decreaseButton = this.addRenderableWidget(new Button.Builder(
-				Component.literal("-"),
-				button -> this.decreaseReach()
-		).pos(SLIDER_X, SLIDER_Y).width(40).height(SLIDER_HEIGHT).build());
+        this.enabledCheckbox = CheckboxWidget.builder(
+                Text.literal("Reach Mod Enabled"),
+                this.textRenderer
+        ).pos(SLIDER_X, 40)
+         .checked(ReachConfig.enabled)
+         .build();
 
-		// Increase Button
-		this.increaseButton = this.addRenderableWidget(new Button.Builder(
-				Component.literal("+"),
-				button -> this.increaseReach()
-		).pos(SLIDER_X + SLIDER_WIDTH + 10, SLIDER_Y).width(40).height(SLIDER_HEIGHT).build());
+        this.addDrawableChild(this.enabledCheckbox);
 
-		// Done Button
-		this.addRenderableWidget(new Button.Builder(
-				Component.literal("Done"),
-				button -> this.onClose()
-		).pos(this.width / 2 - 50, this.height - 30).width(100).build());
-	}
+        this.decreaseButton = this.addDrawableChild(ButtonWidget.builder(
+                Text.literal("-"),
+                button -> this.decreaseReach()
+        ).dimensions(SLIDER_X, SLIDER_Y, 40, SLIDER_HEIGHT).build());
 
-	@Override
-	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-		this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
-		super.render(guiGraphics, mouseX, mouseY, partialTick);
+        this.increaseButton = this.addDrawableChild(ButtonWidget.builder(
+                Text.literal("+"),
+                button -> this.increaseReach()
+        ).dimensions(
+                SLIDER_X + SLIDER_WIDTH + 10,
+                SLIDER_Y,
+                40,
+                SLIDER_HEIGHT
+        ).build());
 
-		// Draw reach distance label and value
-		guiGraphics.drawCenteredString(this.font, 
-			"Reach Distance: " + this.sliderValue + " blocks", 
-			SLIDER_X + SLIDER_WIDTH / 2 + 25, 
-			SLIDER_Y + 5, 
-			0xFFFFFF);
+        this.addDrawableChild(ButtonWidget.builder(
+                Text.literal("Done"),
+                button -> this.close()
+        ).dimensions(this.width / 2 - 50, this.height - 30, 100, 20).build());
+    }
 
-		// Draw range info
-		guiGraphics.drawString(this.font, "Min: 1 | Max: 10", SLIDER_X, SLIDER_Y + 30, 0xAAAAAA);
-	}
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
 
-	private void increaseReach() {
-		if (this.sliderValue < ReachConfig.maxReach) {
-			this.sliderValue++;
-			ReachConfig.setReachDistance(this.sliderValue);
-		}
-	}
+        context.drawCenteredTextWithShadow(
+                this.textRenderer,
+                Text.literal("Reach Distance: " + this.sliderValue + " blocks"),
+                SLIDER_X + SLIDER_WIDTH / 2 + 25,
+                SLIDER_Y + 5,
+                0xFFFFFF
+        );
 
-	private void decreaseReach() {
-		if (this.sliderValue > ReachConfig.minReach) {
-			this.sliderValue--;
-			ReachConfig.setReachDistance(this.sliderValue);
-		}
-	}
+        context.drawTextWithShadow(
+                this.textRenderer,
+                Text.literal("Min: 1 | Max: 10"),
+                SLIDER_X,
+                SLIDER_Y + 30,
+                0xAAAAAA
+        );
+    }
 
-	@Override
-	public void onClose() {
-		ReachConfig.enabled = this.enabledCheckbox.selected();
-		ReachConfig.setReachDistance(this.sliderValue);
-		super.onClose();
-	}
+    private void increaseReach() {
+        if (this.sliderValue < ReachConfig.maxReach) {
+            this.sliderValue++;
+            ReachConfig.setReachDistance(this.sliderValue);
+        }
+    }
 
-	@Override
-	public boolean isPauseScreen() {
-		return false;
-	}
-}
+    private void decreaseReach() {
+        if (this.sliderValue > ReachConfig.minReach) {
+            this.sliderValue--;
+            ReachConfig.setReachDistance(this.sliderValue);
+        }
+    }
+
+    @Override
+    public void close() {
+        ReachConfig.enabled = this.enabledCheckbox.isChecked();
+        ReachConfig.setReachDistance(this.sliderValue);
+        super.close();
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+			}
